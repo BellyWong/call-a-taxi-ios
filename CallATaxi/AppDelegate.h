f07d7e9fae61061f3ef065c7dc013c9114db7d58
Doncho Minkov <donchominkov@AcademyMac.local>
2013-12-13 18:28:34 +0200

    Initial Commit

---
+++ CallATaxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CallATaxi
+//
+//  Created by Doncho Minkov on 12/13/13.
+//  Copyright (c) 2013 minkovit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CallATaxi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CallATaxi
+//
+//  Created by Doncho Minkov on 12/13/13.
+//  Copyright (c) 2013 minkovit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
